ad57e85f80e66ca62690b18e9c4b488095f8ae87
Oleg Burov <oleg.burov@outlook.com>
2017-04-04 12:28:08 -0700

    Implement patch.

---
+++ src/Sitecore.Support.155426/Cloud/Xdb/UpdateXdbConnectionStrings.cs
@@ -0,0 +1,43 @@
+﻿using Sitecore.Cloud.Xdb;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines;
+using System;
+using System.Configuration;
+
+namespace Sitecore.Support.Cloud.Xdb
+{
+  public class UpdateXdbConnectionStrings
+  {
+    public void Process(PipelineArgs args)
+    {
+      Log.Info("Sitecore.Support.155426: Starting updating the timeouts for connection strings between Sitecore application and MongoDB instance.", this);
+
+      var connectTimeout = $"?connectTimeoutMS={Configuration.Settings.GetSetting("connectTimeoutMS")};";
+      var socketTimeout = $"?socketTimeoutMS={Configuration.Settings.GetSetting("socketTimeoutMS")};";
+      
+      string[] xDBConnectionStringNames = { "analytics", "tracking.live", "tracking.history", "tracking.contact" };
+
+      foreach (var name in xDBConnectionStringNames)
+      {
+        var connectionStringValue = ConfigurationManager.ConnectionStrings[name].ConnectionString;
+
+        connectionStringValue = connectionStringValue.Replace("?", connectTimeout);
+        connectionStringValue = connectionStringValue.Replace("?", socketTimeout);
+
+        try
+        {
+          using (var connectionStringsManager = new ConnectionStringsManager())
+          {
+            connectionStringsManager.Update(name, connectionStringValue);
+
+            Log.Info($"Sitecore.Support.155426: The '{name}' connection string has been updated: {connectionStringValue}", this);
+          }
+        }
+        catch (Exception e)
+        {
+          Log.Info($"Sitecore.Support.155426: The 'connectTimeoutMS' and 'socketTimeoutMS' have not been updated. Exception: {e.Message}", this);
+        }
+      }      
+    }
+  }
+}
---
+++ src/Sitecore.Support.155426/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.155426")]
+[assembly: AssemblyProduct("Sitecore.Support.155426")]
+[assembly: ComVisible(false)]
